46c5011680d5aeac211abecd327dd8d4ecdb16c9
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-09-21 09:40:55 -0800

    Login component

---
+++ potluck-planner/src/Components/MainForm.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+const blankData = {
+  username: "",
+  password: "",
+};
+
+export default function Form() {
+  const [userData, setUserData] = useState(blankData);
+  const [userList, setUserList] = useState([]);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const submit = (evt) => {
+    evt.preventDefault();
+    addUser();
+    setUserData(blankData);
+    console.log("UserData", userData);
+    console.log("UserList", userList);
+  };
+
+  const addUser = () => {
+    setUserList([...userList, userData]);
+  };
+
+  return (
+    <>
+      <div className="main">
+        <h2>Sign up!</h2>
+        <form onSubmit={submit}>
+          <div className="item">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={userData.username}
+              onChange={change}
+            />
+          </div>
+          <div className="item">
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              value={userData.password}
+              onChange={change}
+            />
+          </div>
+          <div className="item">
+            <button id="subutton">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
