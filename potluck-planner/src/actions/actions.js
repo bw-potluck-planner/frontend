2b88aff571d4fdc522ae3127aea4f8bee867b0d6
Sara <scearc930@gmail.com>
2020-09-22 14:12:58 -0400

    working on rendering to page

--- potluck-planner/src/actions/actions.js
+++ potluck-planner/src/actions/actions.js
@@ -5,6 +5,11 @@ export const POST_DATA = "POST_DATA";
 export const PUT_DATA = "PUT_DATA";
 export const DELETE_DATA = "DELETE_DATA";
 
+export const GET_POTLUCK = "GET_POTLUCK";
+export const POST_POTLUCK = "POST_POTLUCK";
+export const PUT_POTLUCK = "PUT_POTLUCK";
+export const DELETE_POTLUCK = "DELETE_POTLUCK";
+
 export const fetchData = () => dispatch => {
    axiosWithAuth().get("/api/profile")
    .then(res => {
@@ -40,6 +45,46 @@ export const deleteData = (changes, id) => dispatch => {
     axiosWithAuth().delete(`/api/profile/${id}`, changes)
     .then(res => {
         dispatch({type: DELETE_DATA, payload: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const getPotluck = () => dispatch => {
+    axiosWithAuth().get("/api/potluck")
+    .then(res => {
+        dispatch({type: GET_POTLUCK, payload: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const postPotluck = () => dispatch => {
+    axiosWithAuth().get("/api/potluck")
+    .then(res => {
+        dispatch({type: POST_POTLUCK, payload: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const putPotluck = (changes, id) => dispatch => {
+    axiosWithAuth().get(`/api/potluck/${id}`, changes)
+    .then(res => {
+        dispatch({type: PUT_POTLUCK, payload: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export const deletePotluck = (changes, id) => dispatch => {
+    axiosWithAuth().get(`/api/potluck/${id}`, changes)
+    .then(res => {
+        dispatch({type: DELETE_POTLUCK, payload: res.data})
     })
     .catch(err => {
         console.log(err)
--- potluck-planner/src/utils/axiosWithAuth.js
+++ potluck-planner/src/utils/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VybmFtZSIsInN1YmplY3QiOjExLCJpYXQiOjE2MDA3OTIwMTEsImV4cCI6MTYwMDgyMDgxMX0.FXl9dBkZQY_8dnd6ZX8y-qROSAd0tB4Hu8Zo-KnrrG8'
+    const token = localStorage.getItem("token")
     return axios.create({
         headers: {
             Authorization: token
