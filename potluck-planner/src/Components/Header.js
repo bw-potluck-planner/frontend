68d68a8aedfc4ed6b1cd5895b5e0fac736d6f2f2
Sara <scearc930@gmail.com>
2020-09-24 12:22:56 -0400

    adjusted visual of login buttons

--- potluck-planner/src/Components/Header.js
+++ potluck-planner/src/Components/Header.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link, useHistory } from "react-router-dom";
+
+
 export default function Header() {
+ 
   let history = useHistory();
+
+  
   return (
     <>
       <header className="head">
         <h1 onClick={() => history.push("/")}>Potluck Planner</h1>
         <nav>
-          <Link to="/login">Log-In</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/protected">Profile</Link>
+          {localStorage.getItem("token") === null ? <Link to="/login">Log-In</Link> : <Link to="/protected">Profile</Link>}
+          
+          {localStorage.getItem("token") === null ? <Link to="/register">Register</Link> : <Link to="/login">Logout</Link>}
         </nav>
       </header>
     </>
