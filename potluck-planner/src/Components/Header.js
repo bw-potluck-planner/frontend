267894679aaaf87c3d51728329147cf96217b59f
Sara <scearc930@gmail.com>
2020-09-24 13:48:18 -0400

    added marketing linked pages

--- potluck-planner/src/Components/Header.js
+++ potluck-planner/src/Components/Header.js
@@ -3,9 +3,13 @@ import { Link, useHistory } from "react-router-dom";
 
 
 export default function Header() {
- 
   let history = useHistory();
+  const marketing1 = `https://eat-plan-potluck.netlify.app/`
+  const marketing2 = `https://potluck-paradise.netlify.app/`
 
+  const arrayOfMark = [marketing1, marketing2]
+
+  const marketing = arrayOfMark[Math.floor(Math.random() * arrayOfMark.length)]
   
   return (
     <>
@@ -15,6 +19,7 @@ export default function Header() {
           {localStorage.getItem("token") === null ? <Link to="/login">Log-In</Link> : <Link to="/protected">Profile</Link>}
           
           {localStorage.getItem("token") === null ? <Link to="/register">Register</Link> : <Link to="/login">Logout</Link>}
+          <a href={marketing}>Marketing</a>
         </nav>
       </header>
     </>
